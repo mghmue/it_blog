5ff5056fb55a1214ee5cb97b2ae4d28d73129495
mgmhue <mghmue2000@gmail.com>
2025-06-04 19:41:57 +0630

    php pj

--- backend/sidebar.php
+++ backend/sidebar.php
@@ -35,7 +35,7 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="../backend/">
+                <a class="nav-link" href="../backend/category/post.">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Posts</span></a>
             </li>
